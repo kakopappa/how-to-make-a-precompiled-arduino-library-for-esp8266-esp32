b4d4f759978309f3ed0949822dd443f222445849
Aruna Tennakoon <aruna.tennakoon@gmail.com>
2022-07-27 08:48:25 +0700

    Update hello.cpp

--- lib/hello.cpp
+++ lib/hello.cpp
@@ -3,7 +3,3 @@
 int add(int x, int y) {
  return x + y;
 } 
-
-// C:\Users\user\AppData\Local\Arduino15\packages\esp32\tools\xtensa-esp32-elf-gcc\gcc8_4_0-esp-2021r2-patch3\bin
-// xtensa-esp32-elf-gcc.exe -c hello.cpp -o hello.o
-// xtensa-esp32-elf-ar.exe crf hello.a hello.o
